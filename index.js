c02580c6a8f30b6b489a68abb0e29799a0547c8b
Jeffery Shivers <jefferyshivers@gmail.com>
2020-07-06 18:01:41 -0400

    delete stuff

--- index.js
+++ index.js
@@ -1,37 +1,11 @@
-const grpc = require('@grpc/grpc-js')
-const protoLoader = require('@grpc/proto-loader')
-const { createPrompt, cowsay } = require('./lib')
+const { createPrompt } = require('./lib')
 
-const PROTO_PATH = __dirname + '/.protop/path/protop/pingpong/api.proto'
+const { openPrompt, closePrompt } = createPrompt({
+  'ping': () => openPrompt()
+})
 
-protoLoader.load(PROTO_PATH)
-  .then(packageDefinition => {
-    const protos = grpc.loadPackageDefinition(packageDefinition)
-    const pingPongClient = new protos.protop.pingpong.PingPongService(
-      'localhost:8080',
-      grpc.credentials.createInsecure()
-    )
+process.on('SIGINT', () => {
+  closePrompt()
+})
 
-    const exchange = pingPongClient.exchange()
-
-    const { openPrompt, closePrompt } = createPrompt({
-      'ping': () => exchange.write({/* Ping */})
-    })
-
-    exchange.on('data', data => {
-      cowsay('pong')
-      openPrompt()
-    })
-
-    exchange.on('end', () => {
-      closePrompt()
-      process.exit(0)
-    })
-
-    process.on('SIGINT', () => {
-      closePrompt()
-      exchange.end()
-    })
-
-    openPrompt()
-  })
+openPrompt()
